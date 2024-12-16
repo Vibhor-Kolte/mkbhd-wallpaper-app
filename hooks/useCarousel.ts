41768eb12f6d2f1d834b2665a6078ddcfc695576
Vibhor-Kolte <vibhorkolte@gmail.com>
2024-12-16 16:43:25 +0530

    [ft_add_carousal] - add carousal images

--- hooks/useCarousel.ts
+++ hooks/useCarousel.ts
@@ -2,13 +2,13 @@
 export function useCarousel(): {title: string, image: string;}[] {
     return [
         {
-            image: "https://ideogram.ai/assets/progressive-image/balanced/response/grtS8AsZRGyLm3VJ2SHS7g", 
+            image: "https://ideogram.ai/assets/image/lossless/response/q8QGB2M-SH60xllDwVdQkQ", 
             title: "In the Sunset"
         }, {
             title: "Close to me", 
-            image: "https://ideogram.ai/assets/progressive-image/balanced/response/xPCpOvQYSVqIjuo2mjBPmg"
+            image: "https://ideogram.ai/assets/progressive-image/balanced/response/_q0iK4xuSw-P15anEhmicA"
         }, {
-            image: "https://ideogram.ai/assets/progressive-image/balanced/response/CNhPzxJ1RO2hO8RXF4dang", 
+            image: "https://ideogram.ai/assets/progressive-image/balanced/response/CKY8MFV_Sui5rjjDGfxaVg", 
             title: "In here"
         }]
 }
